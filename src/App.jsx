9aaaac919793e2e9f42244a9d5b2af88f619ec5a
nasrat <nasrat.noury@gmail.com>
2020-10-27 14:23:57 +0100

    test

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,16 @@ import React from 'react';
 import './App.css';
 import { Route, Switch } from 'react-router-dom';
 import Home from './components/Home/Home';
-import PageStyle from './components/PageStyle/PageStyle';
+// import PageStyle from './components/PageStyle/PageStyle';
+// import Style from './components/PageStyle/Style';
 
 function App() {
   return (
     <div className="App">
       <Switch>
         <Route exact path="/" component={Home} />
-        <Route path="/PageStyle" component={PageStyle} />
+        {/* <Route path="/style/:id" component={Style} />
+        <Route path="/PageStyle" component={PageStyle} /> */}
       </Switch>
     </div>
   );
--- src/components/PageStyle/PageStyle.jsx
+++ src/components/PageStyle/PageStyle.jsx
@@ -1,17 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 
-class PageStyle extends React.Component {
+class PageStyle extends Component {
+  constructor() {
+    super();
+    this.state = {
+      listofStyles: [],
+    };
+  }
+
   componentDidMount() {
     axios
       .get('https://api-festit-09-20.herokuapp.com/api/styles')
-      .then((response) => response.data);
+      .then((response) => this.setState({ listofStyles: response.data }));
   }
 
   render() {
+    const { listofStyles } = this.state;
+
     return (
       <div>
         <p> Page Style </p>
+        {listofStyles.map((style) => (
+          <Link to={`/style/${style.idstyle}`}> {style.name} </Link>
+        ))}
       </div>
     );
   }
