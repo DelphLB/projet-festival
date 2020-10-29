9b60fc974cb860d9cdc04fc5b81f1f84dd427a4a
vylis <vylis@users.noreply.github.com>
2020-10-29 17:00:47 +0100

    home first merge

--- src/components/Reusable/NavBar/Search.jsx
+++ src/components/Reusable/NavBar/Search.jsx
@@ -1,17 +1,38 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../../CSS/Reusable/NavBar/Search.css';
 import * as AiIcons from 'react-icons/ai';
+import ReactSearchBox from 'react-search-box';
+import axios from 'axios';
 
 function Search() {
   const [click, setClick] = useState(false);
+  // const [queryInput, setQueryInput] = useState('');
+  const [dataArray, setDataArray] = useState([]);
+  const getData = () => {
+    axios
+      .get('https://api-festit-09-20.herokuapp.com/api/festivals')
+      .then((response) => response.data)
+      .then((data) => {
+        setDataArray(data);
+      });
+  };
+  useEffect(() => {
+    getData();
+  }, []);
+  // const handleInputChange = (event) => {
+  //   setQueryInput(event.target.value);
+  // };
   return (
-    <div>
+    <div className="search-icon">
       <AiIcons.AiOutlineSearch onClick={() => setClick(!click)} />
-      <input
-        className={click ? 'search_active' : 'search'}
-        text="area"
-        placeholder="    Recherche un festival , un artist .."
-      />
+      <div className={click ? 'search_active' : 'search'}>
+        <ReactSearchBox
+          placeholder=" Entrer un artiste , un festival ect .."
+          value=""
+          data={dataArray}
+        />
+        {/* // onChange={(e) => console.log(e)}) */}
+      </div>
     </div>
   );
 }
