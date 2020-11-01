c648ee8dc062385c3af8272abdb0b22cd25bbe50
Delphine Le Brun <delphine.clebrun@gmail.com>
2020-11-01 17:55:12 +0100

    description et route chgt

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,15 @@ import React from 'react';
 import './App.css';
 import { Route, Switch } from 'react-router-dom';
 import Home from './components/Home/Home';
-import PageFestival from './components/PageFestival/PageFestival';
+// import PageFestival from './components/PageFestival/PageFestival';
+import BoxFest from './components/PageFestival/BoxFest';
 
 function App() {
   return (
     <div className="App">
       <Switch>
         <Route exact path="/" component={Home} />
-        <Route path="/festivals/:idfestival" component={PageFestival} />
+        <Route path="/festivals/:idfestival" component={BoxFest} />
       </Switch>
     </div>
   );
--- src/components/PageFestival/BoxFest.jsx
+++ src/components/PageFestival/BoxFest.jsx
@@ -11,10 +11,10 @@ class BoxFest extends Component {
   }
 
   componentDidMount() {
-    // const { idfestival } = this.props.match.params; //ici pour le params j'imagine que c'est pas this mais.pagestyle ???
+    const { idfestival } = this.props.match.params; //ici pour le params j'imagine que c'est pas this mais.pagestyle ???
     axios
-      //   .get(`https://api-festit-09-20.herokuapp.com/api/festivals/${idfestival}`)
-      .get(`https://api-festit-09-20.herokuapp.com/api/festivals/`)
+      .get(`https://api-festit-09-20.herokuapp.com/api/festivals/${idfestival}`)
+      //   .get(`https://api-festit-09-20.herokuapp.com/api/festivals/`)
       .then((response) => {
         this.setState({
           festivals: response.data[0],
@@ -26,12 +26,18 @@ class BoxFest extends Component {
     const { festivals } = this.state;
 
     return (
-      <div
-        className="boxFestival"
-        // styles={{ backgroundImage: `url(${festivals.image1})` }}
-      >
-        <h1 className="styleName">{festivals.name}</h1>
-        <img src={festivals.image1} alt={festivals.name} />
+      <div className="festival">
+        <div
+          className="boxFestival"
+          // styles={{ backgroundImage: `url(${festivals.image1})` }}
+        >
+          <h1 className="styleName">{festivals.name}</h1>
+
+          <img src={festivals.image1} alt={festivals.name} />
+        </div>
+        <div className="descriptionFestival">
+          <p className="descriptionText">{festivals.description}</p>
+        </div>
       </div>
     );
   }
