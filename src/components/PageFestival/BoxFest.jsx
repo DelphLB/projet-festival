5db2570645aff55451225f36605b1e78c90f5818
Delphine Le Brun <delphine.clebrun@gmail.com>
2020-11-03 10:14:15 +0100

    line Up  pack ticket navbar footer

--- src/components/PageFestival/BoxFest.jsx
+++ src/components/PageFestival/BoxFest.jsx
@@ -2,13 +2,16 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import '../CSS/festivals/BoxFest.css';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import Navbar from '../Reusable/NavBar/Navbar';
+import Footer from '../Reusable/Footer/Footer';
 
 class BoxFest extends Component {
   constructor() {
     super();
     this.state = {
       festivals: {},
-      //   tickets: {},
+      tickets: [],
       artists: [],
     };
   }
@@ -24,16 +27,15 @@ class BoxFest extends Component {
         });
       });
 
-    //  axios
-    //   .get(
-    //     `https://api-festit-09-20.herokuapp.com/api//tickets/festivals/${idfestival}`
-    //   )
-    //   .then((response) => {
-    //     console.log(response);
-    //     this.setState({
-    //       tickets: response.data[0],
-    //     });
-    //   });
+    axios
+      .get(
+        `https://api-festit-09-20.herokuapp.com/api//tickets/festivals/${idfestival}`
+      )
+      .then((response) => {
+        this.setState({
+          tickets: response.data,
+        });
+      });
 
     axios
       .get(
@@ -48,10 +50,11 @@ class BoxFest extends Component {
 
   render() {
     const { festivals } = this.state;
-    //  const { tickets } = this.state;
+    const { tickets } = this.state;
     const { artists } = this.state;
     return (
       <div className="festival">
+        <Navbar />;{/* box festival : image en background et nom festoch */}
         <div
           className="boxFestival"
           //    styles={{ backgroundImage: `url(${festivals.image1})` }}
@@ -60,13 +63,39 @@ class BoxFest extends Component {
           <img src={festivals.image1} alt={festivals.name} />
         </div>
         <div className="descriptionFestival">
-          <p className="descriptionText">
-            {festivals.description}
-            {artists.map((artist) => (
-              <div> {artist.name}</div>
-            ))}
-          </p>
+          <p className="descriptionText">{festivals.description}</p>
         </div>
+        {/* le lineup */}
+        <div className="lineUp">
+          {' '}
+          {artists.map((artist) => (
+            <div> {artist.name}</div>
+          ))}
+        </div>
+        {/* package festival (box ticket) */}
+        <div className="packFestival">
+          <div className="cardsPack">
+            {tickets.map((pack) => (
+              <div className="packCadre">
+                <h3>{pack.name}</h3>
+                <p>{pack.description}</p>
+                <p>{pack.date}</p>
+                <p>{pack.price}€</p>
+                {/* lien vers la résa  */}
+                <Link
+                  to="/"
+                  className="bouttonResa"
+                  style={{
+                    textDecoration: 'none',
+                  }}
+                >
+                  <p>Réserver</p>
+                </Link>
+              </div>
+            ))}
+          </div>
+        </div>
+        <Footer />
       </div>
     );
   }
