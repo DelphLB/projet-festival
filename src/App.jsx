fa812d1223515d2120a812f31bbe0b19903ecf3f
nasrat <nasrat.noury@gmail.com>
2020-11-05 17:25:26 +0100

    paymentpage

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Home from './components/Home/Home';
 import PageStyle from './components/PageStyle/PageStyle';
+import PaymentPage from './components/PaymentPage/PaymentPage';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/pageStyle/:idStyle" component={PageStyle} />
+        <Route path="/PaymentPage" component={PaymentPage} />
       </Switch>
     </div>
   );
