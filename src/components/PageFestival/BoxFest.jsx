5daac11116d6e99c57024dde385a986312b22572
Bastien Perrot <bastien.perrot1@gmail.com>
2020-11-13 22:13:03 +0100

    modif page festival centrage icones

--- src/components/PageFestival/BoxFest.jsx
+++ src/components/PageFestival/BoxFest.jsx
@@ -89,7 +89,7 @@ const BoxFest = ({ match, location }) => {
         <div className="descriptionFestival">
           <p className="details">
             <p className="fleche"> &darr; </p>
-            <p className="description">Description</p>
+            <p className="description">DESCRIPTION</p>
             <p className="fleche"> &darr; </p>
           </p>
           {isToggleOn ? (
