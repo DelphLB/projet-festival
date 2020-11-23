3aaddbf50ec2fce1a1f49ae33f35315b0b8db4dd
Bastien Perrot <bastien.perrot1@gmail.com>
2020-11-23 16:25:32 +0100

    merge failed

--- src/components/PageArtiste/ListeArtiste.jsx
+++
@@ -1,7 +0,0 @@
-// import axios from 'axios';
-// import React, { useState, useEffect } from 'react';
-
-// function ListeArtiste() {
-//   return <div></div>;
-// }
-// export default ListeArtiste;
